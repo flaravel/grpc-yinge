7b72926d1d857ba24c439b821bb4e8b9f1953232
atai <atai@yinge.tech>
2021-07-02 10:48:14 +0800

    add new task api

--- src/Finance/MerchantClient.php
+++ src/Finance/MerchantClient.php
@@ -503,4 +503,18 @@ class MerchantClient extends \Grpc\BaseStub {
         $metadata, $options);
     }
 
+    /**
+     * 新建账单导出任务
+     * @param \Yinge\Grpc\Finance\NewExportTaskRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     */
+    public function NewExportTask(\Yinge\Grpc\Finance\NewExportTaskRequest $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/finance.Merchant/NewExportTask',
+        $argument,
+        ['\Yinge\Grpc\Finance\EmptyResponse', 'decode'],
+        $metadata, $options);
+    }
+
 }
