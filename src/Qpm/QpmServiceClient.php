de34b8b6ffe109d803abd18e11a0fb1f8aab903d
kk <290813340@qq.com>
2021-07-06 18:50:24 +0800

    自运营电商订单添加merchantId属性

--- src/Qpm/QpmServiceClient.php
+++ src/Qpm/QpmServiceClient.php
@@ -510,9 +510,9 @@ class QpmServiceClient extends \Grpc\BaseStub {
      * @param array $options call options
      * @return \Yinge\Grpc\Qpm\Response
      */
-    public function FixMerchantId(\Yinge\Grpc\Qpm\BatchUpdateMerchantRequest $argument,
+    public function FixOrderMerchantId(\Yinge\Grpc\Qpm\BatchUpdateMerchantRequest $argument,
       $metadata = [], $options = []) {
-        return $this->_simpleRequest('/Qpm.QpmService/FixMerchantId',
+        return $this->_simpleRequest('/Qpm.QpmService/FixOrderMerchantId',
         $argument,
         ['\Yinge\Grpc\Qpm\Response', 'decode'],
         $metadata, $options);
